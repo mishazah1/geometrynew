ee61cb31ffefca6576d6c76a272d84e27ef3795e
mishazah1 <mishazah2001@mail.ru>
2019-05-22 12:04:53 +0700

    Update project.c

--- src/project.c
+++ src/project.c
@@ -12,22 +12,40 @@ typedef struct figure {
 
 int main()
 {
-    int r, a, b, c, pt;
-    float pc, sc, st, x, y;
+    int r, i, j, flag = 0, r1, r2, x, y, x1, y1, x2,
+                 y2; // x, y, z, x1=0, y1=0, r1=0, x2, y2, r2;
+    float p, s;
+    circle mass[3];
+    printf("Enter 3 circles: \n");
+    for (i = 0; i < 3; i++) {
+        printf("\nEnter x,y,radius of circle: ");
+        scanf("%d %d %d", &x, &y, &r);
+        p = PerimetrC(r, PI);
+        s = SquareC(r, PI);
+        mass[i].x = x;
+        mass[i].y = y;
+        mass[i].r = r;
+        x1 = mass[i].x;
+        y1 = mass[i].y;
+        r1 = mass[i].r;
+        printf("Perimetr: %f Square: %f\n", p, s);
+    }
 
-    printf("\nEnter x,y,radius of circle: ");
-    scanf("%f %f %d", &x, &y, &r);
-    pc = PerimetrC(r, PI);
-    sc = SquareC(r, PI);
-
-    printf("Perimetr: %f Square: %f\n", pc, sc);
-
-    printf("\nEnter a,b,c for triangle: ");
-    scanf("%d %d %d", &a, &b, &c);
-    pt = PerimetrT(a, b, c);
-    st = SquareT(a, b, c);
-
-    printf("Perimetr: %d Square: %f\n", pt, st);
-
+    for (i = 0; i < 2; i++) {
+        x1 = mass[i].x;
+        y1 = mass[i].y;
+        r1 = mass[i].r;
+        for (j = 1; j < 3; j++) {
+            x2 = mass[j].x;
+            y2 = mass[j].y;
+            r2 = mass[j].r;
+            if (j == i) {
+                continue;
+            }
+            flag = peres(x1, y1, r1, x2, y2, r2);
+            if (flag)
+                printf("\nCircle %d crosses circle %d\n", j + 1, i + 1);
+        }
+    }
     return 0;
 }
